3268397ca9c81f709483c98005ec9a3af6cde3f8
Drew Benson <Drewbenson16@gmail.com>
2023-12-28 18:54:51 -0700

    spicy

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('After DOM Loaded');
+    const paragraph = document.getElementById('text');
+    paragraph.textContent = 'This is really cool!';
+  });
